8dfd890a576b79fc867f15e738fd0f42db70c26c
Jason Koch <jkoch@netflix.com>
2021-06-05 23:59:36 -0700

    disable leak suspects view until it can be made more performant with large dumps

--- frontend/src/components/heapdump/HeapDump.vue
+++ frontend/src/components/heapdump/HeapDump.vue
@@ -73,7 +73,7 @@
                             @setSelectedObjectId="setSelectedObjectId"
                   />
                 </el-tab-pane>
-
+<!--
                 <el-tab-pane name="leakSuspects" lazy>
                   <span slot="label">{{$t('jifa.heap.leakSuspects')}}</span>
                   <leak-suspects :file="file"
@@ -81,7 +81,7 @@
                                  @incomingRefsOfObj="incomingRefsOfObj"
                                  @setSelectedObjectId="setSelectedObjectId"/>
                 </el-tab-pane>
-
+-->
                 <el-tab-pane name="GCRoots" lazy>
                   <span slot="label">{{$t('jifa.heap.GCRoots')}}</span>
                   <div v-bind:style="{ 'height': '100%', 'width': resultDivWidth}">
