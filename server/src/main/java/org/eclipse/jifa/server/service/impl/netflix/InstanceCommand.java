b4920135cf2ae5197e259e84a2d74ef1e304a498
Jason Koch <jkoch@netflix.com>
2024-05-09 17:18:01 -0700

    push in netflix services

---
+++ server/src/main/java/org/eclipse/jifa/server/service/impl/netflix/InstanceCommand.java
@@ -0,0 +1,49 @@
+package org.eclipse.jifa.server.service.impl.netflix;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InstanceCommand {
+    final String instanceId;
+    final String commandId;
+    final long pid;
+
+    final static Pattern VALID_REGEX = Pattern.compile("[0-9a-z-]+");
+
+    public InstanceCommand(final String instanceId, final String commandId, final long pid) {
+        this.instanceId = instanceId;
+        this.commandId = commandId;
+        this.pid = pid;
+    }
+
+    public static InstanceCommand parseParam(String param) {
+        if (param == null) {
+            return null;
+        }
+
+        final String[] parts = param.split("!");
+
+        if (parts.length != 4 || !parts[0].equals("s3")) {
+            return null;
+        }
+
+        // validate the data; three requirements:
+        // 1) instance id should be only alpha and hyphen
+        // 2) command id is a uuid
+        Matcher instanceIdMatcher = VALID_REGEX.matcher(parts[1]);
+        Matcher commandIdMatcher = VALID_REGEX.matcher(parts[2]);
+        if (!instanceIdMatcher.matches() || !commandIdMatcher.matches()) {
+            return null;
+        }
+
+        // 3) pid should be parsable as a number (only)
+        final long pid;
+        try {
+            pid = Long.valueOf(parts[3]);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
+        return new InstanceCommand(parts[1], parts[2], pid);
+    }
+}
---
+++ server/src/main/java/org/eclipse/jifa/server/service/impl/netflix/NetflixGandalfUserAccessService.java
@@ -0,0 +1,133 @@
+package org.eclipse.jifa.server.service.impl.netflix;
+
+import static org.eclipse.jifa.common.enums.CommonErrorCode.ILLEGAL_ARGUMENT;
+import static org.eclipse.jifa.server.enums.ServerErrorCode.ACCESS_DENIED;
+import static org.eclipse.jifa.server.enums.ServerErrorCode.FILE_NOT_FOUND;
+import static org.eclipse.jifa.server.enums.ServerErrorCode.UNAVAILABLE;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.eclipse.jifa.common.domain.exception.ErrorCode;
+import org.eclipse.jifa.common.util.Validate;
+import org.eclipse.jifa.server.domain.entity.shared.file.BaseFileEntity;
+import org.eclipse.jifa.server.domain.entity.shared.user.UserEntity;
+import org.eclipse.jifa.server.enums.ServerErrorCode;
+import org.eclipse.jifa.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+import com.netflix.gandalf.agent.AuthorizationClient;
+import com.netflix.gandalf.agent.GandalfException;
+import com.netflix.gandalf.agent.protogen.AuthorizationResponse;
+import com.netflix.metatron.ipc.security.MetatronSslContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Primary
+@Component
+@Slf4j
+public class NetflixGandalfUserAccessService implements FileAccessService {
+
+    @Value("${fc.flamecommanderApi}")
+    private String flamecommanderApi;
+
+    AuthorizationClient authorizationClient;
+
+    @Autowired
+    UserService userService;
+
+    public NetflixGandalfUserAccessService() {
+        this.authorizationClient = new AuthorizationClient();
+        this.authorizationClient.status();
+    }
+
+    @Override
+    public void checkAuthorityForCurrentUser(BaseFileEntity file)
+    {
+        UserEntity user = userService.getCurrentUser();
+
+        log.info("Checking authority for file '{}' and user '{}'",
+            file.getUniqueName(),
+            user.getName());
+
+        InstanceCommand ic = InstanceCommand.parseParam(file.getUniqueName());
+        Validate.notNull(ic, ILLEGAL_ARGUMENT, "Not an s3! formed pathname: " + file.getUniqueName());
+
+        // look up historic information in flamecommander database for instance
+        final FcEvent fcEvent;
+        try {
+            fcEvent = loadFlamecommanderEvent(ic);
+        } catch (IOException e) {
+            log.error("flamecommander api error, sending error", e);
+            Validate.error(FILE_NOT_FOUND, "flamecommander api error: " + e.getMessage());
+            return;
+        }
+
+        Validate.notNull(fcEvent, FILE_NOT_FOUND,
+            String.format("No event found in flamecommander for instanceId=%s, commandId=%s",
+                ic.instanceId, ic.commandId));
+
+        Validate.notNull(user.getName(), ACCESS_DENIED, "No user identity found");
+
+        log.debug("found identity, verifying resources against gandalf");
+        JsonObject subject = createGandalfSubject(user.getName());
+        JsonObject resource = createGandalfResource();
+        try
+        {
+            AuthorizationResponse authorizationResponse = authorizationClient.isAuthorized(fcEvent.application,
+                            fcEvent.stack, fcEvent.accountId, fcEvent.region, subject, "SSH", resource, null);
+            log.debug("gandalf response: {}", authorizationResponse);
+            Validate.isTrue(authorizationResponse.getAllowed(), ACCESS_DENIED, "Access denied by gandalf");
+        }
+        catch (GandalfException e)
+        {
+            Validate.error(UNAVAILABLE, "gandalf error: " + e.getMessage());
+        }
+
+    }
+
+    static class FcEvent {
+        @SerializedName("account_id")
+        String accountId;
+        String application;
+        String stack;
+        String region;
+    }
+
+    String getFcProfileLookupUrl(InstanceCommand ic) {
+        return flamecommanderApi + "api/jifa/fc-event/" + ic.instanceId + "/" + ic.commandId;
+    }
+
+    FcEvent loadFlamecommanderEvent(InstanceCommand ic) throws IOException {
+        final String eventUrl = getFcProfileLookupUrl(ic);
+        HttpsURLConnection connection = (HttpsURLConnection) new URL(eventUrl).openConnection();
+        connection.setSSLSocketFactory(MetatronSslContext.forClient("flamecommander").getSocketFactory());
+        connection.setHostnameVerifier((hostname, session) -> true);
+        FcEvent[] events = new Gson().fromJson(new InputStreamReader(connection.getInputStream()), FcEvent[].class);
+        return (events.length == 1) ? events[0] : null;
+    }
+
+    JsonObject createGandalfSubject(String email) {
+        final JsonObject user = new JsonObject();
+        user.addProperty("email", email);
+        final JsonObject subject = new JsonObject();
+        subject.add("user", user);
+        return subject;
+    }
+
+    JsonObject createGandalfResource() {
+        final JsonObject resource = new JsonObject();
+        resource.addProperty("username", "nfsuper");
+        return resource;
+    }
+
+}
